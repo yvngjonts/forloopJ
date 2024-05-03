52dbb5a4178e386dd68236850eab927562b8dd1f
opilane <opilane@HP-CZC0359NN8>
2024-05-03 12:17:54 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace forloopJ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("For loop");
+
+            int loop = 2;
+
+            for (int i = 0; i < loop; i++)
+            {
+                Console.WriteLine("Loop " + i);
+                Console.Beep();
+            }
+        }
+    }
+}
